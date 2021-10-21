bb752f0284d61fda2a38a2d6f359c6386df1aba5
Hüsnü Taş <husnutas@outlook.com>
2021-10-21 17:23:06 +0300

    Initial Commit

---
+++ HW5_HusnuTas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW5_HusnuTas
+//
+//  Created by Hüsnü Taş on 21.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
